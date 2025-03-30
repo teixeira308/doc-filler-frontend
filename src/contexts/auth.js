c7520526ebfd06e27a0bd239f911250f5b5e3c30
Guilherme Teixeira <teixeira308@hotmail.com>
2025-03-30 13:58:15 -0300

    fix

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -15,7 +15,7 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   const signin = (email, password) => {
-    return fetch("${apiUrl}/users/login", {
+    return fetch(`${apiUrl}/users/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
