c45903b2d2b38579f3cd3efd5ff18248c14199d6
Guilherme Teixeira <teixeira308@hotmail.com>
2025-04-04 00:12:44 -0300

    add logs

--- src/services/apiTemplates.js
+++ src/services/apiTemplates.js
@@ -155,7 +155,8 @@ const ApiTemplates = () => {
 
     }
 
-    const generateBatchDocuments = async (payload) => {
+    const generateBatchDocuments = async (payload) => {\
+        console.log(payload)
     const response = await fetch(`${apiUrl}/fill-docx-template/batch`, {
         method: "POST",
         headers: {
